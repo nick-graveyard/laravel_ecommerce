ff479eca47100108b9191281fcf94cb29464cb05
vagrant <vagrant@homestead>
2015-11-27 17:05:12 +0000

    first commit, add hello_world route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,19 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+// Route::get('/', 'WelcomeController@index');
+
+// Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+
+Route::get('hello_world', function () {
+    return 'Hello World';
+});
+
+
